b45b316a10d8329989f462afb8045cc865a1585f
Vinicius Sodré <vni217@gmail.com>
2024-11-19 14:41:23 -0300

    teste de upload de imagens

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+const isProd = process.env.NODE_ENV === "production";
+
+const nextConfig = {
+  basePath: isProd ? "/lpt" : "",
+  assetPrefix: isProd ? "/lpt/" : "",
+  images: {
+    unoptimized: true, // Desabilita a otimização de imagens (requisito no GitHub Pages)
+  },
+};
+
+module.exports = nextConfig;
