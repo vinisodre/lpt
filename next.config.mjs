c9f821c96b691964f60830e794bcf8af1480bea2
Vinicius Sodré <vni217@gmail.com>
2024-11-19 14:57:21 -0300

    back

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === "production";
+const nextConfig = {};
 
-const nextConfig = {
-  basePath: isProd ? "/lpt" : "",
-  assetPrefix: isProd ? "/lpt/" : "",
-  images: {
-    unoptimized: true, // Desabilita a otimização de imagens
-  },
-};
-
-export default nextConfig; // Use export default para compatibilidade com ES Modules
+export default nextConfig;
